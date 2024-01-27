f30c14a6cd7361bfeba36eeeef49a502f0cfb944
Wonder Jung <wonder13662@gmail.com>
2024-01-27 23:37:33 +0900

    일련 파일 생성 스크립트 추가

---
+++ utils/2.10.36_ko/create_pages/index.js
@@ -0,0 +1,74 @@
+const fs = require('node:fs/promises');
+
+// 1. 새로 만들 페이지 목록 정의
+const pageFileNameTemplate = '90-01-10-windowsx-02-dockable_dialogsx-${page_no_two_digits}-${page_name_in_file}.md'
+const pages = [
+  {
+    page_file_name: 'tool_options',
+    page_name_ko: '도구 옵션',
+    page_name_en: 'Tool Options',
+  },
+  {
+    page_file_name: 'device_status',
+    page_name_ko: '장치 상태',
+    page_name_en: 'Device Status',
+  },
+  {
+    page_file_name: 'layers',
+    page_name_ko: '레이어',
+    page_name_en: 'Layers',
+  },  
+]
+
+const getPageNo = (idx) => `90.1.10.2.${idx + 1}.`
+const getPageNo2Digits = (idx) => `${idx + 1}`.padStart(2, '0')
+const getPageFileName = (idx) => idx > -1 ? pageFileNameTemplate.replace('${page_no_two_digits}', getPageNo2Digits(idx)).replace('${page_name_in_file}', pages[idx].page_file_name) : ''
+const getPageFullName = (idx) => `${getPageNo(idx)} ${pages[idx].page_name_ko}(${pages[idx].page_name_en})`
+
+const doAsyncJob = async () => {
+  // 2. 샘플 페이지 정보 읽어오기
+  const path = __dirname + '/output'
+
+  try {
+    // 2-1. 샘플 페이지 파일 정보 읽어오기
+    let data = await fs.readFile('./page_template.md', 'utf8')
+    
+    // 2-2. 목록 아이템 별 페이지 작성하기
+    for (let idx = 0; idx < pages.length; idx++) {
+      // 2-2-1. 파일 이름 만들기
+      const pageFileName = getPageFileName(idx)
+
+      // 2-2-2. 파일 내용 바꾸기
+      let content = data.replaceAll('${page_no}', getPageNo(idx)).replaceAll('${page_name_ko}', pages[idx].page_name_ko).replaceAll('${page_name_en}', pages[idx].page_name_en)
+
+      // 2-2-3. 이전 페이지 추가하기
+      const hasPrev = idx > 0
+      const prevIdx = idx - 1
+      // (예시) page_name_prev: 90.1.10.2.1. 도구 옵션
+      const page_name_prev = hasPrev ? getPageFullName(prevIdx) : ''
+      content = content.replace('${page_name_prev}', page_name_prev)
+      // (예시) page_prev: ./90-01-10-windowsx-02-dockable_dialogsx-01-tool_options.md
+      const page_prev = hasPrev ? `./${getPageFileName(prevIdx)}` : ''
+      content = content.replace('${page_prev}', page_prev)
+
+      // 2-2-4. 다음 페이지 추가하기
+      const hasNext = idx < pages.length - 1
+      const nextIdx = idx + 1
+      // (예시) page_name_next: 90.1.10.2.3. 레이어
+      const page_name_next = hasNext ? getPageFullName(nextIdx) : ''
+      content = content.replace('${page_name_next}', page_name_next)
+      // (예시) page_next: ./90-01-10-windowsx-02-dockable_dialogsx-03-layers.md
+      const page_next = hasNext ? `./${getPageFileName(nextIdx)}` : ''
+      content = content.replace('${page_next}', page_next)
+
+      // 2-2-5. 새로운 페이지 파일 추가하기
+      const targetPath = pageFileName
+      await fs.writeFile(targetPath, content);
+    }    
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+doAsyncJob()
+
---
+++ utils/2.10.36_ko/create_pages/page_template.md
@@ -0,0 +1,31 @@
+# [90. 이미지 맵](./90-00-image-map.md)
+# [90.1. 이미지 메뉴 바(Image Menu Bar)](./90-01-00-image-menu-bar.md)
+# [90.1.10. 창(Windows)](./90-01-10-windows.md)
+# [90.1.10.2. 도킹가능한 대화(Dockable dialogs)](./90-01-10-windowsx-02-dockable_dialogs.md)
+# ${page_no} ${page_name_ko}(${page_name_en})
+## 운영체제(OS)와 언어설정 조건에 따른 모습
+#### 그림 ${page_no}a101. `창` → `${page_name_ko}` (Windows)
+(TODO 이미지 추가)
+
+#### 그림 ${page_no}a102. `창` → `${page_name_ko}` (Mac)
+(TODO 이미지 추가)
+
+***
+
+## 관련 정보
+
+***
+
+## 다른 페이지로 가기
+
+[➡️ 다음: ${page_name_next}](${page_next})
+
+[⬅️ 이전: ${page_name_prev}](${page_prev})
+
+[⬆️ 위: 90.1.10.2. 도킹가능한 대화(Dockable dialogs)](./90-01-10-windowsx-02-dockable_dialogs.md)
+
+[⬆️ 위: 90.1.10. 창(Windows)](./90-01-10-windows.md)
+
+[⬆️ 위: 90.1. 이미지 메뉴 바(Image Menu Bar)](./90-01-00-image-menu-bar.md)
+
+[🏠 홈](./00-home.md)
